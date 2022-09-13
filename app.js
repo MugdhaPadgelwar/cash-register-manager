5bac03802226066c619cc680d33dafc5caaa3c2f
Mansi Padgelwar <mugdhapadgelwar2002@gmail.com>
2022-09-13 16:44:51 +0530

    updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkBtn = document.querySelector("#check-btn");
+const nextBtn = document.querySelector("next-button");
 const message = document.querySelector("#error-message");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 const numberOfNotes = document.querySelectorAll(".no-of-notes");
